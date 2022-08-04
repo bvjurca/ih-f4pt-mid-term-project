9544bbb4c6001679435a49419a019ab8a28bc193
bogdinamitawhoami <bogdifromsalem@yahoo.com>
2022-08-04 22:59:41 +0200

    Dark mode improvements, minor form improvements, ultraminor JS logic added

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var darkModeOn = false;
 
-// dark mode
+// dark mode conversion
 document.getElementById("mode-toggle").onclick = function darkMode() {
   const el = document.body;
   el.classList.toggle("dark-mode");
@@ -21,28 +21,33 @@ document.getElementById("mode-toggle").onclick = function darkMode() {
 
   const art = document.querySelectorAll("article");
   art.forEach((el) => {
-    el.classList.toggle("dark-mode")
-  })
+    el.classList.toggle("dark-mode");
+  });
 
   const head4 = document.querySelectorAll("h4");
   head4.forEach((el) => {
-    el.classList.toggle("dark-mode")
-  })
+    el.classList.toggle("dark-mode");
+  });
 
   const head2 = document.querySelectorAll("h2");
   head2.forEach((el) => {
-    el.classList.toggle("dark-mode")
-  })
+    el.classList.toggle("dark-mode");
+  });
 
   const btn = document.querySelectorAll(".btn");
   btn.forEach((el) => {
-    el.classList.toggle("dark-logo")
-  })
+    el.classList.toggle("dark-logo");
+  });
 
   const logos = document.querySelectorAll("#logostrip img");
   logos.forEach((el) => {
-    el.classList.toggle("dark-logo")
-  })
+    el.classList.toggle("dark-logo");
+  });
+
+  const links = document.querySelectorAll("article a");
+  links.forEach((el) => {
+    el.classList.toggle("dark-logo");
+  });
 
   const head1 = document.querySelector("h1");
   head1.classList.toggle("h1dark");
@@ -53,28 +58,52 @@ document.getElementById("mode-toggle").onclick = function darkMode() {
   logo2.classList.toggle("dark-logo");
 
   darkModeOn = true;
-
-}
-
-// sticky navi
-window.addEventListener('scroll',(e)=>{
-    const nav = document.querySelector('nav');
-    if(window.pageYOffset>0){
-      nav.classList.add("add-shadow");
-      if (darkModeOn) {
-        nav.style.boxShadow = "0px 10px 20px rgba(146, 147, 153, 0.1)";
-      }
-    }else{
-      nav.classList.remove("add-shadow");
-      nav.style.boxShadow = 'null';
-    }
-
-    
-
-  });
-
-  //contact btn
-document.getElementById("btn-main").onclick = function () {
-    location.href = "/contact.html"; 
 };
 
+
+
+// sticky navi
+window.addEventListener("scroll", (e) => {
+  const nav = document.querySelector("nav");
+  if (window.pageYOffset > 0) {
+    nav.classList.add("add-shadow");
+    if (darkModeOn) {
+      nav.style.boxShadow = "0px 10px 20px rgba(146, 147, 153, 0.1)";
+    }
+  } else {
+    nav.classList.remove("add-shadow");
+    nav.style.boxShadow = "null";
+  }
+});
+
+
+
+
+//contact btn
+document.getElementById("btn-main").onclick = function () {
+  location.href = "/contact.html";
+};
+
+
+//build the projectpage
+const links = document.querySelectorAll("#projects article");
+links.forEach((el) => {
+
+  const title = el.querySelector("h4").innerHTML;
+  console.log(title);
+  const tag = el.querySelector("p").innerHTML;
+  console.log(tag);
+
+  const link = el.querySelector("a");
+  return link.addEventListener("click", projDetailPage, false);
+});
+
+function projDetailPage() {
+  document.querySelector("#project-detail h1").innerHTML = title;
+  document.querySelector("#project-meta h4").innerHTML = tag;
+  
+}
+
+function formSubmit(){
+  alert ("Form submitted");
+}
