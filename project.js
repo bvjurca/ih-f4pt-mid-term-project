2eea0f09fb4a633bb54748504a9f4c53cb45b4dc
bogdinamitawhoami <bogdifromsalem@yahoo.com>
2022-08-09 23:23:15 +0200

    JS improvements, richer CSS hovers, nicer dark mode

--- project.js
+++ project.js
@@ -1,16 +1,17 @@
 window.onload = () => {
-    var params = new Proxy(new URLSearchParams(window.location.search), {
-        get: (searchParams, prop) => searchParams.get(prop),
-      });
+  var params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
 
-      //turn params into something easier to work with
-      var { title, tag, imgPath } = params;
-      
-      document.querySelector('#project-detail h1').textContent = title;
-      document.querySelector('h4').textContent = tag;
-      const images = document.querySelectorAll('#project-detail img');
-      images.forEach((el) => {
-        el.src = imgPath;
-      });
+  //turn params into something easier to work with
+  var { title, tag, imgPath } = params;
+
+  document.querySelector("#project-detail h1").textContent = title;
+  document.querySelector("h4").textContent = tag;
+
+  //replace images with incoming path
+  const images = document.querySelectorAll("#project-detail img");
+  images.forEach((el) => {
+    el.src = imgPath;
+  });
 };
-
